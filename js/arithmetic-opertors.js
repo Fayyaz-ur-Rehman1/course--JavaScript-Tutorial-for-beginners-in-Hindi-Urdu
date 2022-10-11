ccba744793edc433cf97ae33f750b58061292d19
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-12 02:01:13 +0530

    js-of-arithmetic-opretors

---
+++ js/arithmetic-opertors.js
@@ -0,0 +1,118 @@
+document.write("<h1>Arithmetic Operators</h1>")
+document.write("<h1>1.<u>Addition +</h1></u>")
+var a = 5;
+var b = 6 ;
+var c = a + b;
+
+document.write(c);
+document.write("<br>")
+
+var a = 71;
+var b = 17;
+var c = a + b;
+
+document.write(c)
+
+document.write("<h1>2.<u>Subtraction -</h1></u>")
+var d = 54
+var e = 45
+var f = d- e;
+
+document.write(f)
+document.write("<br>")
+
+var g = 100;
+var h = 30;
+var i = g-h;
+
+document.write(i)
+
+document.write("<h1>3.<u>Multiplication</u></h1>")
+var j = 70 
+var k = 100
+var l = j * k;
+
+document.write(l)
+document.write("<br>")
+
+var m = 5
+var n = 3
+var o = m * n
+
+document.write(o)
+
+document.write("<h1>4.<u>Exponentiation</u></h1>")
+var p = 10
+var q = 11
+var r = p ** q
+
+document.write(r)
+document.write("<br>")
+
+var s = 3
+var t = 4
+var w = s**t
+
+document.write(w)
+document.write("<br>")
+
+document.write("<h1>5.<u>Division</u></h1>")
+var x = 4
+var Y = 2
+var z = x / Y
+
+document.write(z)
+document.write("<br>")
+
+var xx = 40
+var yy = 2
+var zz = xx / yy 
+
+document.write(zz)
+
+document.write("<h1>6.<u>Modulus(Remainder)</u></h1>")
+var aa = 40
+var bb = 2
+var  cc = aa % bb
+
+document.write(cc)
+document.write("<br>")
+
+var dd = 40
+var ee = 7
+var ff = dd % ee 
+
+document.write(ff)
+
+document.write("<h1>7.<u>Increment</u></h1>")
+var a  = 5
+var b = 1
+document.write(a+b)
+document.write("<br>")
+a++;
+document.write(a + b);
+document.write("<h5>Example</h5>")
+
+var f = 10
+var s = 5
+document.write(f + s)
+document.write("<br>")
+s++;
+document.write(f + s )
+
+
+document.write("<h1>8.<u>Decrement</u></h1>")
+var ii = 7
+var jj = 9
+
+document.write(ii - jj)
+document.write("<br>")
+ii--;
+document.write(ii-jj)
+document.write("<h5>Example</h5>")
+var kk = 10
+var ll = 10
+document.write(kk-ll)
+document.write("<br>")
+kk--;
+document.write(kk-ll)
