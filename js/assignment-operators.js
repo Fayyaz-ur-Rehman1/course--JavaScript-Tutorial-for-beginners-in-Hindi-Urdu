0d32f1e91bd99e97506bbbf3934193c2a8e2976d
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-12 15:48:43 +0530

    js-of-assignment-operators file

---
+++ js/assignment-operators.js
@@ -0,0 +1,83 @@
+document.write("<h1>Assignment-Operators</h1>")
+document.write("<h1>1.<u>Equal-to =</u></h1>")
+var a = 30
+var b = 40
+var c = a+b;
+document.write(c)
+document.write("<br>")
+
+var d = 15
+var e = 26
+var f = d+e
+document.write(f)
+
+document.write("<h1>2.<u>Addition-equal-to +=</u></h1>")
+var g = 6
+var h = 8
+g += h;
+document.write(g)
+document.write("<br>")
+
+var i = 15
+var j = 15
+i += j
+document.write(i)
+
+document.write("<h1>3.<u>Subtraction-equal-to -=</u></h1>")
+var k = 18
+var l = 5
+k -= l 
+document.write(k)
+document.write("<br>")
+
+var m = 4
+var n = 5
+m -= n ;
+document.write(m)
+
+document.write ("<h1>4.<u>multiplication-equal-to *=</u></h1>")
+var o = 8
+var p = 3
+o *= p
+document.write(o)
+document.write("<br>")
+
+var q = 7
+var s = 4
+q *= s
+document.write(q)
+
+document.write("<h1>5.<u>Exponentiation-equal-to **=</u></h1>")
+var t = 2
+var w = 3
+t **= w
+document.write(t)
+document.write("<br>")
+
+var x = 4
+var y = 5
+x **= y
+document.write(x)
+document.write("<h1>6.<u>Division-equal-to /=</u></h1>")
+var z = 15
+var aa = 3
+z /= aa 
+document.write(z)
+document.write("<br>")
+
+var cc = 5
+var dd = 79
+cc /= dd
+document.write(cc)
+
+document.write("<h1>7.<u>Modulus-equal-to %=</u></h1>")
+var ee = 15
+var jj = 3
+ee %= jj 
+document.write(ee)
+document.write("<br>")
+
+var hh = 5
+var mm = 79
+hh /= mm 
+document.write(hh)
