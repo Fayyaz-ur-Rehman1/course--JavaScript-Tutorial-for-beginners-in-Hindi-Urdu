f9ddcc93ccdab7a7fe4e06ea18ba158f5490543a
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-13 16:00:08 +0530

    js-for-if-statement-file

---
+++ js/if-statement.js
@@ -0,0 +1,31 @@
+document.write("<h1>If-Statement</h1>")
+var a = 90
+var h = 8
+if(a > h){
+    document.write("a is greater")
+}
+document.write("<br>")
+var b = 7
+var k = 8
+if(b < k){
+    document.write("k is smaller")
+}
+document.write("<br>")
+var c = 19
+var l = 19
+if (c == l){
+    document.write("FAYYAZ-KHILJI")
+}
+document.write("<br>")
+var d = 4
+var f = 6
+if(d == f){
+    document.write("this is word answer beacuse this value is not equal-this value is worng then not show out put")
+}
+document.write("<br>")
+
+var d = "4"
+var n = "4"
+if(d === n){
+    document.write( "yes data type")
+}
