990cc16f1817030fabda3e0e135ac67f9da65896
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-17 22:46:20 +0530

    js-for-logical-operators-file

---
+++ js/logical-operatoers.js
@@ -0,0 +1,50 @@
+document.write("<h1>Logical-operators</u>")
+document.write("<h1>1. <u>Logical-End-Operators</u></h1>")
+var age = 20
+if(age >= 18 && age <= 21) {
+ document.write("normal-child")
+}
+document.write("<br>")
+var mobile = 26
+ if(mobile >= 25 && mobile <= 27){
+  document.write("First-Rank")
+ }
+ document.write("<br>")
+ var FAYYAZ = 100
+if(FAYYAZ > 50 && FAYYAZ < 200){
+    document.write("fayyaz is good boy")
+}
+document.write("<br>")
+var name = 30
+if (name > 36 && name < 37){
+ document.write("my name is fayyaz ur rehman ")
+}
+document.write("<h1>2.<u>Logical-Or-Opreators</u></h1>")
+var number = 39
+if(number > 10|| number < 35){
+ document.write("number is true")
+}
+document.write("<br>")
+var age = 50
+if(age >= 50 || age <= 39){
+    document.write("my name is fayyaz khilji ")
+}
+document.write("<br>")
+var present = 40
+if(present > 35 || present < 50){
+document.write("adnan chohan")
+}
+document.write("<br")
+var leptop = 60
+if(leptop == 4 || leptop == 50){
+document.write("ayyaz")
+}
+document.write("<br>")
+document.write("<h1><u>Logical-Not-Operators</u></h1>")
+var fayyaz = 70 
+var danish = 10
+  console.log(fayyaz < danish)
+/*
+if(!fayyaz <= 700 ){
+document.write("yes u are eligible")
+}*/
