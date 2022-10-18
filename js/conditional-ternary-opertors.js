50283135ac937a91773df3cb77620122b7be2274
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-18 19:20:45 +0530

    js-for-conditionl-ternary-opertors-js

---
+++ js/conditional-ternary-opertors.js
@@ -0,0 +1,13 @@
+document.write("<h1>Conditional-Ternary-Operators</h1><br>")
+
+var a = 40
+b = (a == 40)? "true": "false";
+document.write(b)
+document.write("<br>")
+var m = 50
+b = (m < 40)? "true":"false";
+document.write(b)
+document.write("<br>")
+var n = 60
+o = (n < 60)? "true":"fasle";
+document.write(o)
