a8037e1f84519b8c1a2f57103005c76243c3d589
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-19 13:29:43 +0530

    js-for-if-eles-if-statement-file

---
+++ js/if-eles-if-statement.js
@@ -0,0 +1,18 @@
+document.write("<h1>If-Else-If-Statement</h1>")
+var b = 45
+if(b <= 2 && b >= 2 ){
+    document.write("this is true")
+}else if (b  > 40 && b < 221){
+  document.write("this is a false")
+}else{
+  document.write("this is not true and false")
+}
+document.write("<br>")
+var c = 6
+if (c > 3 && c < 12){
+    document.write("vale is true")
+}else if (c < 1 && c < 12){
+    document.write("value is false")
+}else{
+    document.write("snot answer")
+}
