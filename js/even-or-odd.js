7040629ac977eda55e2aa81b3a0bc0aa1ea779f6
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-11-30 22:10:16 +0530

    js-for-even-or-odd-md-file

---
+++ js/even-or-odd.js
@@ -0,0 +1,12 @@
+for(var e = 1 ; e <= 30 ; e++){
+    if(e % 2 == 0){
+       document.write(e+"<br>")
+    }
+   }
+   document.write("<hr>")
+
+   for( var w = 4 ; w <= 40 ; w++ ){
+    if( w % 4 == 0){
+        document.write(w + "<br>")
+    }
+   }
