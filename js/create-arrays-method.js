21a888fccd109898c587fe805d032b07bda3471c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-11-30 22:30:51 +0530

    js-for-arrays-js-file

---
+++ js/create-arrays-method.js
@@ -0,0 +1,27 @@
+// var g = new Array(5)
+// for(var r = 0 ; r < 5 ;r++){
+//     g[r] = prompt("hlw i am fayyaz-ur-rehman")
+// }
+// document.write(g+ "<br>")
+
+// var h = new Array(3)
+// h[0]= "fayyaz"
+// h[1]="danish"
+// h[2]="uzair"
+// h[3]="ayyaz"
+// h[4]="arbaz"
+// for(var t = 0 ; t < 1; t++){
+//     h[4] = prompt("s")
+// }
+// document.write(h + "<br>")
+
+ var h = new Array()
+ h[0]= "fayyaz"
+ h[1]="danish"
+ h[2]="uzair"
+ h[3]="ayyaz"
+ h[4]="arbaz"
+ document.write("<ul>")
+ for(var t = 0 ; t < 5; t++){ document.write("<li>" + h[t] + "</li>")
+ }
+ document.write("<ul>")
