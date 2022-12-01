6765fccc51557681fd6c8143e1c2be23d347e6be
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-01 21:14:48 +0530

    js-for-modify-and-delete-file

---
+++ js/modify-and-delete.js
@@ -0,0 +1,14 @@
+var a = ["fayyaz", 17 , "11th"]
+document.write(a + "<br>")
+
+a[0] = "danish"
+document.write(a + "<br>")
+a[2] = " "
+document.write(a + "<br>")
+delete a[2]
+document.write(a + "<br>")
+
+var x = ["muktiyar","ikbal","ayan","afan"]
+document.write(x + "<br>")
+x[0] = "salam"
+document.write(x + "<br>")
