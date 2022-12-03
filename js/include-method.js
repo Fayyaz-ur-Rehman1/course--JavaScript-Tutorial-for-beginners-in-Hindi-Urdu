c75e97b680aee5d2fa0ba0654fbb312cbc8fe0c5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-03 11:19:51 +0530

    js-for-include-method-file

---
+++ js/include-method.js
@@ -0,0 +1,9 @@
+var a = ["fayyaz","danish","uzair","ayyaz"]
+
+document.write(a + "<br>")
+
+var b = a.includes("fayyaz")
+document.write(b + "<br>")
+
+var c = a.includes("ayyaz1")
+document.write(c)
