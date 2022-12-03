c6acb33de144dc6ef0b7c907c206b1c594eea3a2
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-03 11:12:43 +0530

    js-for-concat-and-join-file

---
+++ js/concat-and-join.js
@@ -0,0 +1,12 @@
+var a = ["my name is fayyaz"]
+document.write(a + "<br>")
+var b = ["i am a student of softerwere developper"]
+document.write(b + "<br>")
+
+document.write("<hr>")
+
+var c = a.concat(b)
+document.write(c + "<br>")
+
+var d = c.join(" ")
+document.write(d + "<br>")
