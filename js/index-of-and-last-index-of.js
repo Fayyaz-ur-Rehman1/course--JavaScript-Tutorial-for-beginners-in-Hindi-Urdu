2cd78c8f2a781326f089620bb8c5d536564e514e
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-03 11:16:34 +0530

    js-for-index-of-and-last-index-of-file

---
+++ js/index-of-and-last-index-of.js
@@ -0,0 +1,11 @@
+var a = ["danish","fayyaz","uzair","fayyaz","ayyaz"]
+document.write(a + "<br>")
+
+var b = a.indexOf("fayyaz",2)
+document.write(b + "<br>")
+
+var c = ["danish","fayyaz","uzair","fayyaz","ayyaz"]
+document.write(c + "<br>")
+
+var d = c.lastIndexOf("fayyaz",2)
+document.write(d + "<br>")
