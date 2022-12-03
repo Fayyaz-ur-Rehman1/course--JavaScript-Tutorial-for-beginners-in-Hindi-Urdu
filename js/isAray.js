beca00e5533f222bbe71c032d09a9fdfcacb4464
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-03 11:14:53 +0530

    js-for-is-array-file

---
+++ js/isAray.js
@@ -0,0 +1,30 @@
+var a = ["danish","fayyaz"]
+var b = "danish fayyaz"
+var c = 10
+document.write(a + "<br>")
+document.write(b + "<br>")
+document.write(c + "<br>")
+document.write( "<hr>")
+ var d = Array.isArray(a)
+ document.write(d + "<br>")
+
+document.write("<hr>")
+
+if(Array.isArray(a) == true){
+    document.write("yes this is an array")
+}else{
+    document.write("no this is not")
+}
+document.write( "<hr>")
+
+var i = ["fayyazurrehman"]
+var o = "fayyaz"
+var b = 14
+document.write(i + "<br>")
+document.write(o + "<br>")
+document.write(b + "<br>")
+
+document.write("<hr>")
+var l = Array.isArray(i)
+document.write(l + "<br>")
+document.write("<hr>")
