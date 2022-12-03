48b0adc02f00a27dfdff7dcfcb526ccb6d6cb850
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-03 11:13:43 +0530

    js-for-slice-and-splice-file

---
+++ js/slice-and-splice.js
@@ -0,0 +1,11 @@
+var a = ["danish","uzair","fayyaz","ayyaz"]
+document.write(a + "<br>")
+var b = a.slice(0,3)
+document.write(b + "<br>")
+
+var c = ["washing","power","lower","nirma-soap"]
+document.write(c + "<br>")
+c.splice(2,1,"rajesh")
+document.write(c + "<br>")
+
+// (index, hmd , nvalue)
