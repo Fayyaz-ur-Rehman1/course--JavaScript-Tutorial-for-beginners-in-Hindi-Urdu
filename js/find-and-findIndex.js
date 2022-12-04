235fce8458d1051042ec7536a339c7d5419dc057
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-04 22:26:34 +0530

    js-for-find-and-find-index-file

---
+++ js/find-and-findIndex.js
@@ -0,0 +1,23 @@
+var a = [10,20,30,400]
+
+document.write(a + "<br>")
+
+var b = a.find(fay)
+
+
+ function fay(dan){
+    return dan < 100
+ }
+ document.write(b)
+
+ document.write("<hr>")
+ var c = [1111,20,30,400]
+
+document.write(c + "<br>")
+
+var d = c.findIndex(faya)
+
+document.write(d)
+ function faya(dani){
+    return dani < 111
+ }
