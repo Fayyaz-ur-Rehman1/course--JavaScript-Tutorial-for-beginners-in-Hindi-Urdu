6477ac126c91bec3541d0d6118c3152a535ca451
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-05 23:29:19 +0530

    array-of-object

---
+++ js/array-of-objects.js
@@ -0,0 +1,11 @@
+var a = [
+    {name : "my name is fayyaz", age : "18"},
+    {name : "my name is danish " , age : "17"},
+]
+console.log(a)
+document.write(a)
+for(var b = 0 ; b < a.length; b++){
+    document.write(a[b].name + a[b].age + "<br>")
+}
+
+
