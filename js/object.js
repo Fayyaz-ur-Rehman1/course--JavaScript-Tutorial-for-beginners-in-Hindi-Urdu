a1b02dbe69f2181c5e655f60f7fea397cf5f66f7
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-05 23:26:57 +0530

    js-for-object-file

---
+++ js/object.js
@@ -0,0 +1,10 @@
+var a = {
+    fayyaz : "my full name is fayyaz ur rehman",
+    danish : "full name is danish chohan",
+    freind : {
+       "1st" : "uziar",
+       "2nd" : "danish", 
+    }
+}
+console.log(a)
+document.write(a.freind["1st"])
