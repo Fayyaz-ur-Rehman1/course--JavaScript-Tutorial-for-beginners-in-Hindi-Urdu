23f6809a193a58dd4752781e14a45d39dd18cec6
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-07 22:17:06 +0530

    js-for-for-in-loop-file

---
+++ js/for-in-loop.js
@@ -0,0 +1,27 @@
+var a = {
+    
+    name : "Danish",
+    age15 : 17,
+    class14 : "11th",
+    email13 : "danishchouhan41@gmai.com",
+    name12 : "Danish",
+    age11 : 17,
+    class0 : "11th",
+    email9 : "danishchouhan41@gmai.com",
+    name8 : "Danish",
+    age7 : 17,
+    class6 : "11th",
+    email5 : "danishchouhan41@gmai.com",
+    name4 : "Danish",
+    age3 : 17,
+    class2: "11th",
+    email1 : "danishchouhan41@gmai.com",
+    
+    
+}
+
+
+ 
+for(var b in a){
+    document.write(b + " : " + a[b] + "<br>")
+}
