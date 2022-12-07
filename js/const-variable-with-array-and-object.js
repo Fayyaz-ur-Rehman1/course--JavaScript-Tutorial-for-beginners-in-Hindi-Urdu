ebc6433302b9d6415df567546c61636a39f52718
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-07 22:15:39 +0530

    js-for-const-variable-with-array-and-object-file

---
+++ js/const-variable-with-array-and-object.js
@@ -0,0 +1,19 @@
+const a = [1,2,3,4,5,6,7,8,9,10]
+
+a[2] = 4
+a[3] = 5
+a[4] = 6
+
+console.log(a)
+
+const b = {
+    name : "danish",
+    age : "17"
+
+}
+b.name = "fayyaz"
+b.age = 18  
+
+
+
+console.log(b)
