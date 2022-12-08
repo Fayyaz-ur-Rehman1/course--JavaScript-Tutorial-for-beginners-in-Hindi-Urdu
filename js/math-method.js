2fbf1b4e0ef0cfa94cba550bf04913a373c556ed
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-08 16:36:54 +0530

    js-for-math-method-file

---
+++ js/math-method.js
@@ -0,0 +1,41 @@
+document.write("ceil-method<br>")
+var a = Math.ceil(2.1);
+document.write(a)
+document.write("<hr>")
+
+document.write("floor-method<br>")
+var a = Math.floor(100.4);
+document.write(a)
+document.write("<hr>")
+
+document.write("round-method<br>")
+var a = Math.round(5.4);
+document.write(a)
+document.write("<hr>")
+
+document.write("trunc-method<br>")
+var a = Math.trunc(4.11);
+document.write(a)
+document.write("<hr>")
+
+
+document.write("max-method<br>")
+var a = Math.max(1,3,5,7,9,10);
+document.write(a)
+document.write("<hr>")
+
+document.write("min-method<br>")
+var a = Math.floor(1,3,5,7,9,10);
+document.write(a)
+document.write("<hr>")
+
+document.write("sqrt-method<br>")
+var a = Math.sqrt(25);
+document.write(a)
+document.write("<hr>")
+
+document.write("cbrt-method<br>")
+var a = Math.cbrt(64);
+document.write(a)
+document.write("<hr>")
+
