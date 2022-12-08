086073ed6dda6c08174b202c9e6497f1578c26c6
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-08 09:15:17 +0530

    js-for-string-method-file

---
+++ js/string-method-second.js
@@ -0,0 +1,82 @@
+document.write("<h1>CharAt-Method</h1>")
+
+var a = "my name is fayyaz"
+var b = a.charAt(0)
+
+document.write(b)
+document.write("<hr>")
+document.write("<h1>charCodeAt-Method</h1>")
+
+var c = "my name is fayyaz"
+var d = c.charCodeAt(1)
+
+document.write(d)
+
+
+document.write("<hr>")
+document.write("<h1>fromCharAT</h1>")
+
+// var e = "my name is fayyaz"
+var e =  String.fromCharCode(121)
+
+document.write(e)
+
+
+document.write("<hr>")
+
+document.write("<h1>concat-Method</h1>")
+
+var f = "my name is fayyaz"
+var ff = "and i m a good boy"
+var g = f.concat(ff)
+
+document.write(g)
+document.write("<hr>")
+
+document.write("<h1>split-Method</h1>")
+
+var h = "my name is fayyaz"
+var i = h.split("a")
+
+document.write(i)
+
+
+document.write("<hr>")
+document.write("<h1>repeat-Method</h1>")
+
+var j = "my name is fayyaz"
+var k = j.repeat(5)
+
+document.write(k)
+
+
+document.write("<hr>")
+
+document.write("<h1>slice-Method</h1>")
+
+var l = "my name is fayyaz"
+var m = l.slice(2,10);
+
+document.write(m)
+
+
+document.write("<hr>")
+
+
+document.write("<hr>")
+
+document.write("<h1>substr-Method</h1>")
+
+var n = "my name is fayyaz"
+var o = n.substr(1);
+
+document.write(o)
+document.write("<hr>")
+
+document.write("<h1>substrig-Method</h1>")
+
+var p = "my name is fayyaz"
+var q = p.substring(1,4);
+
+document.write(q)
+document.write("<hr>")
