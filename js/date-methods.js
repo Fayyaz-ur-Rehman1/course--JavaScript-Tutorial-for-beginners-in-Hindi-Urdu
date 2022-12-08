8afcae26e0f28739e5213c5aae9fd361a480b8cd
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-08 17:34:35 +0530

    js-for-date-method-file

---
+++ js/date-methods.js
@@ -0,0 +1,80 @@
+// document.write("<h1>To-Date-String-Method</h1>")
+// var a =  new Date();
+// document.write(a)
+// document.write("<hr>")
+
+// document.write("<h1>Get-Date-Method</h1>")
+// var a =  new Date();
+// document.write(a.getDate())
+// document.write("<hr>")
+
+// document.write("<h1>Get-full-Year</h1>")
+// var a =  new Date();
+// document.write(a.getFullYear())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Month-Method</h1>")
+// var a =  new Date();
+// document.write(a.getMonth())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Day-Method</h1>")
+// var a =  new Date();
+// document.write(a.getDay())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Hourse-Method</h1>")
+// var a =  new Date();
+// document.write(a.getHours())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Minutes-Method</h1>")
+// var a =  new Date();
+// document.write(a.getMinutes())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Seconds-Method</h1>")
+// var a =  new Date();
+// document.write(a.getSeconds())
+// document.write("<hr>")
+
+// document.write("<h1>Get-Milliisecond-Method-Method</h1>")
+// var a =  new Date();
+// document.write(a.getMilliseconds())
+// document.write("<hr>")
+
+document.write("<h1>Set-Date-Method</h1>")
+var b =  new Date();
+b.setDate(9)
+document.write(b)
+document.write("<hr>")
+
+document.write("<h1>Set-Full-Year</h1>")
+var b =  new Date();
+b.setFullYear(2023)
+document.write(b)
+document.write("<hr>")
+
+document.write("<h1>Set-Hours</h1>")
+var b =  new Date();
+b.setHours(24)
+document.write(b)
+document.write("<hr>")
+
+document.write("<h1>Set-Millisecond-Method</h1>")
+var b =  new Date();
+b.setMilliseconds(10)
+document.write(b)
+document.write("<hr>")
+
+document.write("<h1>Set-Minutes-Method</h1>")
+var b =  new Date();
+b.setMinutes(60)
+document.write(b)
+document.write("<hr>")
+
+document.write("<h1>Set-Month</h1>")
+var b =  new Date();
+b.setMonth(9)
+document.write(b)
+document.write("<hr>")
