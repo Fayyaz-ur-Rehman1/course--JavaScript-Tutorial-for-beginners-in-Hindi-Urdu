3bbdbb144bddbb210e4f77c5c39c21b2a8a016f1
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-08 09:09:08 +0530

    js-for-map-method-file

---
+++ js/map-method.js
@@ -0,0 +1,22 @@
+// var a = [0,2,3,4,5]
+
+// var b = a.map(fay);
+
+// function fay(ur){
+//    return ur * 2
+// }
+// document.write(b)
+
+
+
+var c = [
+    {fname : "fayyaz" , lname : "urrehman"},
+    {fname : "danish" , lname : "chouhan"}
+]
+
+var d = c.map(faya);
+
+function faya(urr){
+   return urr.fname + "-" + urr.lname + "<br>"
+}
+document.write(d)
