7193f8d69bdf9a0930baa935848fc63ecb396814
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-19 17:32:24 +0530

    added-file

--- js/AddHtmlTags.js
+++ js/AddHtmlTags.js
@@ -2,4 +2,4 @@ document.write("Hello world <br>");
 document.write("<i><b>Hello form yahoo baba</b>  </i> ");
 
 document.write("br tag is use break the line this tag is br tag <br> <hr>")
-document.write("<i><b>any html tag in use javascript then tag wirte in double code then work in any Html tag  that is called html tag in javascript</i></b> <br>")
+document.write("<i><b>any html tag in uSe javascript then tag wirte in double code then work in any Html tag  that is called html tag in javascript</i></b> <br>")
