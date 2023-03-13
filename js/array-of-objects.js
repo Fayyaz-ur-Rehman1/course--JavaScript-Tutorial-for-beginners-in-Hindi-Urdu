86284acd622954fb383d99de9122c9130ef9242b
Fayyaz <fayyazurrehman289@gmail.com>
2023-03-14 00:51:34 +0530

    Array-of-Object-add-file

--- js/array-of-objects.js
+++ js/array-of-objects.js
@@ -1,11 +1,9 @@
 var a = [
     {name : "my name is fayyaz", age : "18"},
-    {name : "my name is danish " , age : "17"},
+    {name : "my name is danish " , age : "7"},
 ]
 console.log(a)
 document.write(a)
 for(var b = 0 ; b < a.length; b++){
     document.write(a[b].name + a[b].age + "<br>")
 }
-
-
